370bbca2769b14916a993715a5f85ecde710e964
Aylla <ayllatyfanny16@gmail.com>
2020-05-05 20:55:03 -0300

    alteração no retorno create session

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -4,8 +4,8 @@ module.exports = {
     async create(req, res) {
         const { usuario, senha } = req.body;
 
-        const user = await connection('usuarios').where('usuario', usuario).where('senha', senha).select('usuario').first();
-
+        const user = await connection('usuarios').where('usuario', usuario).where('senha', senha).select('*').first();
+      
         if (!user) {
             return res.status(400).json({ error: 'No USER found with this user and senha.' });
         }
